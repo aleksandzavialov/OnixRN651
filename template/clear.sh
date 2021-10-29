5ce652fe8b1730cf1f14070e00b3609c42834d33
Alexandr Zavialov <alexandr.zavialov@onix-systems.com>
2021-10-30 02:25:33 +0300

    init

--- template/clear.sh
+++ template/clear.sh
@@ -1,23 +1,28 @@
 echo "START <==============="
 echo "start watch dell all <==============="
 watchman watch-del-all
-echo "start delete node modules, install node modules <==============="
+echo "start delete node modules, .lock files <==============="
 sudo rm -rf node_modules \
-&& rm -rf package-lock.json \
-&& rm -rf yarn.lock \
-&& npm install --legacy-peer-deps
+&& sudo rm -rf package-lock.json \
+&& sudo rm -rf yarn.lock
 echo "clean android build <==============="
-sudo rm -rf android/app/build
-echo "start clean ios, install Pods <==============="
-sudo cd ios \
-&& rm -rf ~/Library/Developer/Xcode/DerivedData \
-&& rm -rf Pods \
-&& rm -rf build \
-&& rm -rf Podfile.lock \
-&& pod install \
+cd android \
+&& sudo rm -rf app/build \
+&& sudo rm -rf .gradle \
+&& sudo rm -rf build \
+&& cd ..
+echo "start clean ios <==============="
+cd ios \
+&& sudo rm -rf ~/Library/Developer/Xcode/DerivedData \
+&& sudo rm -rf Pods \
+&& sudo rm -rf Podfile.lock \
+&& sudo rm -rf build \
 && cd ..
 echo "start install RN-modules and POD-modules"
-npm install && cd ios && pod install && cd ..
+npm install --legacy-peer-deps \
+&& cd ios \
+&& pod install \
+&& cd ..
 echo "start project on ios   <==============="
 #react-native run-android --variant=variant --appIdSuffix suffix
 #react-native run-ios --scheme Scheme
